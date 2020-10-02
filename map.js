7b2c1f50a6805a253c9ca78b7db23685a12a1bbb
jacksonjude <jacksonjude@mac.com>
2020-10-01 17:46:01 -0700

    Using timezone adjustment for election day countdown

--- map.js
+++ map.js
@@ -19,6 +19,8 @@ var challengerPartyNum
 var marginColorValues = [15, 5, 1, 0]
 var marginColors = [["#1c408c", "#587ccc", "#8aafff", "#949bb3"], ["#be1c29", "#ff5864", "#ff8b98", "#cf8980"]]
 
+var stateBoxColors = ["#707cff"]
+
 const regionNameToID = {"Alabama":"AL", "Alaska":"AK", "Arizona":"AZ", "Arkansas":"AR", "California":"CA", "Colorado":"CO", "Connecticut":"CT", "Delaware":"DE", "District of Columbia":"DC", "Florida":"FL", "Georgia":"GA", "Hawaii":"HI", "Idaho":"ID", "Illinois":"IL", "Indiana":"IN", "Iowa":"IA", "Kansas":"KS", "Kentucky":"KY", "Louisiana":"LA", "ME-1":"ME-D1", "ME-2":"ME-D2", "Maine":"ME-AL", "Maryland":"MD", "Massachusetts":"MA", "Michigan":"MI", "Minnesota":"MN", "Mississippi":"MS", "Missouri":"MO", "Montana":"MT", "NE-1":"NE-D1", "NE-2":"NE-D2", "NE-3":"NE-D3", "Nebraska":"NE-AL", "Nevada":"NV", "New Hampshire":"NH", "New Jersey":"NJ", "New Mexico":"NM", "New York":"NY", "North Carolina":"NC", "North Dakota":"ND", "Ohio":"OH", "Oklahoma":"OK", "Oregon":"OR", "Pennsylvania":"PA", "Rhode Island":"RI", "South Carolina":"SC", "South Dakota":"SD", "Tennessee":"TN", "Texas":"TX", "Utah":"UT", "Vermont":"VT", "Virginia":"VA", "Washington":"WA", "West Virginia":"WV", "Wisconsin":"WI", "Wyoming":"WY"}
 
 const regionEV = {"AL":9, "AK":3, "AZ":11, "AR":6, "CA":55, "CO":9, "CT":7, "DE":3, "DC":3, "FL":29, "GA":16, "HI":4, "ID":4, "IL":20, "IN":11, "IA":6, "KS":6, "KY":8, "LA":8, "ME-D1":1, "ME-D2":1, "ME-AL":2, "MD":10, "MA":11, "MI":16, "MN":10, "MS":6, "MO":10, "MT":3, "NE-D1":1, "NE-D2":1, "NE-D3":1, "NE-AL":2, "NV":6, "NH":4, "NJ":14, "NM":5, "NY":29, "NC":15, "ND":3, "OH":18, "OK":7, "OR":7, "PA":20, "RI":4, "SC":9, "SD":3, "TN":11, "TX":38, "UT":6, "VT":3, "VA":13, "WA":12, "WV":5, "WI":10, "WY":3}
@@ -53,7 +55,7 @@ var currentMapState = kViewing
 
 var showingHelpBox = false
 
-const electionDayTime = 1604390400000
+const electionDayTime = 1604361600000 //1604390400000 PST
 
 $(function() {
   $(".slider").css("width", (parseInt($("#svgdata").css("width").replace("px", ""))*parseInt(document.getElementById("mapzoom").style.zoom.replace("%", ""))/100-170) + "px")
@@ -693,7 +695,6 @@ function updateStateBox(regionID)
 
   if (currentDataSource == kProjectionSource)
   {
-    //
     regionMarginString += "<br></span><span style='font-size: 17px; padding-top: 5px; padding-bottom: 5px; display: block; line-height: 100%;'>Chances<br>"
     regionMarginString += "<span style='color: " + marginColors[challengerPartyNum][2] + ";'>"
     regionMarginString += decimalPadding(Math.round(regionData.chanceChal*1000)/10)
@@ -709,7 +710,8 @@ function updateStateBox(regionID)
 function updateElectionDayCountdown()
 {
   var currentDate = new Date()
-  var timeUntilElectionDay = electionDayTime-currentDate.getTime()
+  var electionDayLocalOffset = (new Date(electionDayTime)).getTimezoneOffset()*60*1000
+  var timeUntilElectionDay = electionDayTime-currentDate.getTime()+electionDayLocalOffset
 
   var daysUntilElectionDay = Math.floor(timeUntilElectionDay/(1000*60*60*24))
   var hoursUntilElectionDay = Math.floor(timeUntilElectionDay/(1000*60*60)%24)
