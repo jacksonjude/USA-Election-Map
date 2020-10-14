15ddf3932b9a2cea4f146b4f4f672c63827e5d93
jacksonjude <jacksonjude@mac.com>
2020-10-13 21:30:01 -0700

    Added min brightness check to ev pie chart slice labels; r key resizesElements()

--- mapdisplay.js
+++ mapdisplay.js
@@ -34,6 +34,7 @@ var regionMarginStrings = []
 
 var evPieChartCutoutPercent = 50
 const minEVPieChartSliceLabelValue = 16
+const minEVPieChartSliceLabelBrightness = 0.7
 
 $(function() {
   $("#loader").hide()
@@ -58,9 +59,8 @@ function resizeElements(initilizedPieChart)
   var windowWidth = $(window).width()
 
   //1.0*svgdatawidth*zoom/windowwidth == 0.6
-
   var mapZoom = 0.62*windowWidth/$("#svgdata").css("width").replace("px", "")
-  document.getElementById("mapzoom").style.zoom = (mapZoom*100) + "%"
+  $("#mapzoom").css("zoom", (mapZoom*100) + "%")
 
   var mapWidth = parseInt($("#svgdata").css("width").replace("px", ""))*mapZoom
   $(".slider").css("width", mapWidth-190 + "px")
@@ -642,15 +642,14 @@ function setupEVPieChart()
         },
         labelTextColor: function(tooltipItem, chart) {
           var color = chart.config.data.datasets[tooltipItem.datasetIndex].backgroundColor[tooltipItem.index]
-          return color
+          return adjustBrightness(color, minEVPieChartSliceLabelBrightness)
         }
       }
     },
     plugins: {
       datalabels: {
         color: function(context) {
-          var index = context.dataIndex;
-          var value = context.dataset.data[index];
+          var value = context.dataset.data[context.dataIndex]
           return value < minEVPieChartSliceLabelValue ? "rgb(0, 0, 0, 0)" : "#fff"
         },
         font: {
@@ -736,7 +735,6 @@ function updateEVPieChart()
   evPieChart.data.datasets[1].data = partyTotals
 
   var safeMarginTotals = [marginTotals[0], marginTotals[4], marginTotals[8]] // Hardcoding two parties
-  console.log(marginTotals, safeMarginTotals, partyTotals)
   if (safeMarginTotals.toString() == partyTotals.toString())
   {
     evPieChart.data.datasets[0].hidden = true
@@ -956,6 +954,10 @@ document.addEventListener('keypress', async function(e) {
   {
     toggleEditing()
   }
+  else if (e.which == 82)
+  {
+    resizeElements()
+  }
 })
 
 var mouseIsDown = false
@@ -1099,4 +1101,88 @@ function decimalPadding(num)
 function getKeyByValue(object, value)
 {
   return Object.keys(object).find(key => object[key] == value)
+}
+
+function adjustBrightness(hexColorString, minBrightness)
+{
+  var rgb = hexToRGB(hexColorString)
+  if (!rgb) { return }
+
+  var hsv = RGBtoHSV(rgb)
+  if (hsv.v < minBrightness)
+  {
+    hsv.v = minBrightness
+  }
+
+  console.log(hsv.v)
+
+  return RGBToHex(HSVtoRGB(hsv))
+}
+
+function hexToRGB(hex)
+{
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null
+}
+
+function componentToHex(c) {
+  var hex = c.toString(16)
+  return hex.length == 1 ? "0" + hex : hex
+}
+
+function RGBToHex(rgb) {
+  return "#" + componentToHex(rgb.r) + componentToHex(rgb.g) + componentToHex(rgb.b)
+}
+
+function RGBtoHSV(r, g, b) {
+  if (arguments.length === 1) {
+    g = r.g, b = r.b, r = r.r
+  }
+  var max = Math.max(r, g, b), min = Math.min(r, g, b),
+    d = max - min,
+    h,
+    s = (max === 0 ? 0 : d / max),
+    v = max / 255
+
+  switch (max) {
+    case min: h = 0; break
+    case r: h = (g - b) + d * (g < b ? 6: 0); h /= 6 * d; break
+    case g: h = (b - r) + d * 2; h /= 6 * d; break
+    case b: h = (r - g) + d * 4; h /= 6 * d; break
+  }
+
+  return {
+    h: h,
+    s: s,
+    v: v
+  }
+}
+
+function HSVtoRGB(h, s, v) {
+  var r, g, b, i, f, p, q, t
+  if (arguments.length === 1) {
+    s = h.s, v = h.v, h = h.h
+  }
+  i = Math.floor(h * 6)
+  f = h * 6 - i
+  p = v * (1 - s)
+  q = v * (1 - f * s)
+  t = v * (1 - (1 - f) * s)
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break
+    case 1: r = q, g = v, b = p; break
+    case 2: r = p, g = v, b = t; break
+    case 3: r = p, g = q, b = v; break
+    case 4: r = t, g = p, b = v; break
+    case 5: r = v, g = p, b = q; break
+  }
+  return {
+    r: Math.round(r * 255),
+    g: Math.round(g * 255),
+    b: Math.round(b * 255)
+  }
 }
