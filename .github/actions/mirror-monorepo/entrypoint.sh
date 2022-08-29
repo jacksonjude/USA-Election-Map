26ba8c7166365db9c5c1836719087fc1bdc25a78
jacksonjude <jacksonjude@mac.com>
2022-08-28 19:54:25 -0700

    Using custom mirror-monorepo action

---
+++ .github/actions/mirror-monorepo/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -l
+
+USER_EMAIL="$1"
+USER_NAME="$2"
+SOURCE_REPO_PATH="$3"
+SOURCE_BRANCH="$4"
+DEST_REPO_PATH="$5"
+DEST_BRANCH="$6"
+
+set -e  # if a command fails it stops the execution
+set -u  # script fails if trying to access to an undefined variable
+
+if [ -n "${SSH_DEPLOY_KEY:=}" ]
+then
+	mkdir --parents "$HOME/.ssh"
+	DEPLOY_KEY_FILE="$HOME/.ssh/deploy_key"
+	echo "${SSH_DEPLOY_KEY}" > "$DEPLOY_KEY_FILE"
+	chmod 600 "$DEPLOY_KEY_FILE"
+
+	SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
+	ssh-keyscan -H github.com > "$SSH_KNOWN_HOSTS_FILE"
+
+	export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
+else
+	echo "::error::SSH_DEPLOY_KEY is empty."
+fi
+
+git config --global user.email "$USER_EMAIL"
+git config --global user.name "$USER_NAME"
+git clone --single-branch --branch "$DEST_BRANCH" "https://git@github.com/$DEST_REPO_PATH.git"
+cd "$REPO_NAME"
+git fetch "https://github.com/$SOURCE_REPO_PATH.git" "$SOURCE_BRANCH" && git merge FETCH_HEAD --allow-unrelated-histories
+git push -u "https://git@github.com/$DEST_REPO_PATH.git" "$DEST_BRANCH"
