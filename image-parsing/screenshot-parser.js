7c89d5e24d50a952b41a845d30dd264135caef83
jacksonjude <jacksonjude@mac.com>
2024-11-05 13:50:51 -0800

    Support for different map outlines

--- image-parsing/screenshot-parser.js
+++ image-parsing/screenshot-parser.js
@@ -4,7 +4,7 @@ const CSVConverter = require('json-2-csv');
 const fs = require('fs');
 const path = require('path');
 
-const regionCoords = {
+const yapmsRegionCoords = {
 	"AK": [227,118],
 	"AL": [618,145],
 	"AR": [567,206],
@@ -63,6 +63,130 @@ const regionCoords = {
 	"WY": [374,336]
 };
 
+const jacksonjudeRegionCoords = {
+	"AK": [199,119],
+	"AL": [815,240],
+	"AR": [726,296],
+	"AZ": [288,252],
+	"CA": [149,344],
+	"CO": [458,390],
+	"CT": [1111,327],
+	"DC": [1111,201],
+	"DE": [1111,263],
+	"FL": [954,156],
+	"GA": [941,214],
+	"HI": [335,42],
+	"IA": [640,477],
+	"ID": [289,542],
+	"IL": [762,469],
+	"IN": [821,464],
+	"KS": [616,380],
+	"KY": [839,360],
+	"LA": [732,155],
+	"MA": [1097,550],
+	"MD": [1021,444],
+	"ME-AL": [1031,703],
+	"ME-D1": [952,680],
+	"ME-D2": [952,646],
+	"MI": [867,517],
+	"MN": [673,631],
+	"MO": [664,382],
+	"MS": [749,197],
+	"MT": [412,620],
+	"NC": [970,334],
+	"ND": [493,628],
+	"NE-AL": [753,712],
+	"NE-D1": [847,712],
+	"NE-D2": [758,681],
+	"NE-D3": [846,681],
+	"NH": [1102,583],
+	"NJ": [1067,469],
+	"NM": [427,251],
+	"NV": [199,398],
+	"NY": [1004,534],
+	"OH": [922,465],
+	"OK": [552,302],
+	"OR": [171,567],
+	"PA": [1027,486],
+	"RI": [1111,358],
+	"SC": [989,282],
+	"SD": [491,548],
+	"TN": [792,311],
+	"TX": [616,170],
+	"UT": [316,411],
+	"VA": [977,381],
+	"VT": [1111,421],
+	"WA": [199,683],
+	"WI": [713,570],
+	"WV": [926,390],
+	"WY": [425,513]
+};
+
+const twoSeventyRegionCoords = {
+	"AK": [,],
+	"AL": [,],
+	"AR": [,],
+	"AZ": [,],
+	"CA": [,],
+	"CO": [,],
+	"CT": [,],
+	"DC": [,],
+	"DE": [,],
+	"FL": [,],
+	"GA": [,],
+	"HI": [,],
+	"IA": [,],
+	"ID": [,],
+	"IL": [,],
+	"IN": [,],
+	"KS": [,],
+	"KY": [,],
+	"LA": [,],
+	"MA": [,],
+	"MD": [,],
+	"ME-AL": [,],
+	"ME-D1": [,],
+	"ME-D2": [,],
+	"MI": [,],
+	"MN": [,],
+	"MO": [,],
+	"MS": [,],
+	"MT": [,],
+	"NC": [,],
+	"ND": [,],
+	"NE-AL": [,],
+	"NE-D1": [,],
+	"NE-D2": [,],
+	"NE-D3": [,],
+	"NH": [,],
+	"NJ": [,],
+	"NM": [,],
+	"NV": [,],
+	"NY": [,],
+	"OH": [,],
+	"OK": [,],
+	"OR": [,],
+	"PA": [,],
+	"RI": [,],
+	"SC": [,],
+	"SD": [,],
+	"TN": [,],
+	"TX": [,],
+	"UT": [,],
+	"VA": [,],
+	"VT": [,],
+	"WA": [,],
+	"WI": [,],
+	"WV": [,],
+	"WY": [,]
+};
+
+const regionCoordsMap = {
+	'yapms': {coords: yapmsRegionCoords, size: {w: 960, h: 546}},
+	'jacksonjude': {coords: jacksonjudeRegionCoords, size: {w: 1195, h: 756}},
+	'270': {coords: twoSeventyRegionCoords}
+}
+
 const regionEVs = {"AL":9,"AK":3,"AZ":11,"AR":6,"CA":54,"CO":10,"CT":7,"DE":3,"DC":3,"FL":30,"GA":16,"HI":4,"ID":4,"IL":19,"IN":11,"IA":6,"KS":6,"KY":8,"LA":8,"ME":4,"ME-D1":1,"ME-D2":1,"ME-AL":2,"MD":10,"MA":11,"MI":15,"MN":10,"MS":6,"MO":10,"MT":4,"NE":5,"NE-D1":1,"NE-D2":1,"NE-D3":1,"NE-AL":2,"NV":6,"NH":4,"NJ":14,"NM":5,"NY":28,"NC":16,"ND":3,"OH":17,"OK":7,"OR":8,"PA":19,"RI":4,"SC":9,"SD":3,"TN":11,"TX":40,"UT":6,"VT":3,"VA":13,"WA":12,"WV":4,"WI":10,"WY":3}
 
 const margins = {
@@ -87,11 +211,10 @@ const marginToValue = {
 	repTilt:['rep', 0.01]
 };
 
-const originalImageSize = {w: 960, h: 546};
-
 (async () => {
 	const imagePath = process.argv[2] ?? __dirname;
-	const verbose = process.argv[3] ?? false;
+	const mapType = process.argv[3] ?? 'yapms'
+	const verbose = process.argv[4] ?? false;
 	
 	let imageFiles = [imagePath];
 	if (fs.lstatSync(imagePath).isDirectory()) {
@@ -101,18 +224,20 @@ const originalImageSize = {w: 960, h: 546};
 	}
 	
 	for (let imageFile of imageFiles) {
-		processImage(imageFile, verbose);
+		processImage(imageFile, mapType, verbose);
 	}
 })();
 
-async function processImage(imageFilename, verbose) {
-	const image = await Jimp.read(imageFilename);
-	image.resize(originalImageSize);
+async function processImage(imageFilename, mapType, verbose) {
+	const {coords, size} = regionCoordsMap[mapType]
 	
-	const regionResults = parseImageMargins(image);
+	const image = await Jimp.read(imageFilename);
+	image.resize(size);
+	
+	const regionResults = parseImageMargins(image, coords, size);
 	verbose && console.log(regionResults);
 	
-	verbose && await writeDotsImage(image, imageFilename);
+	verbose && await writeDotsImage(image, imageFilename, coords, size);
 	
 	const evByRegion = Object.keys(regionResults)
 		.reduce((results, region) => {
@@ -139,13 +264,13 @@ async function processImage(imageFilename, verbose) {
 	fs.writeFileSync(`${imageFilename}-results.csv`, csvJSON);
 }
 
-function parseImageMargins(image) {
+function parseImageMargins(image, regionCoords, size) {
 	let regionResults = {}
 	
 	for (let region in regionCoords) {
 		if (regionCoords[region].length != 2) continue;
 		
-		const pixelColorHex = image.getPixelColor(regionCoords[region][0], originalImageSize.h-regionCoords[region][1]).toString(16).toUpperCase();
+		const pixelColorHex = image.getPixelColor(regionCoords[region][0], size.h-regionCoords[region][1]).toString(16).toUpperCase();
 		// console.log(pixelColorHex)
 		const margin = Object.keys(margins).reduce((bestMargin, currMargin) =>
 			!bestMargin || colorDistance(margins[bestMargin], pixelColorHex) > colorDistance(margins[currMargin], pixelColorHex) ? currMargin : bestMargin
@@ -157,12 +282,12 @@ function parseImageMargins(image) {
 	return regionResults;
 }
 
-async function writeDotsImage(image, imageFilename) {
+async function writeDotsImage(image, imageFilename, regionCoords, size) {
 	image.greyscale();
 	
 	for (let region in regionCoords) {
 		const centerX = regionCoords[region][0];
-		const centerY = originalImageSize.h-regionCoords[region][1];
+		const centerY = size.h-regionCoords[region][1];
 		const diameter = 5;
 		
 		for (let x = centerX-Math.floor(diameter/2); x < centerX+Math.floor(diameter/2); x += 1) {
